2d2a4db775630677db27791665822c86de9ea405
nuranwael <nuranlwordsjen@gmail.com>
2025-05-02 05:20:44 +0300

    new changes to code

--- public/login.js
+++ public/login.js
@@ -10,7 +10,7 @@ document.getElementById("loginForm").addEventListener("submit", async function (
     }
 
     try {
-        const response = await fetch("http://localhost:5000/api/users/login", {
+        const response = await fetch("https://doclink-production.up.railway.app/login.html", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password })
